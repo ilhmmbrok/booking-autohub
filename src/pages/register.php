d606f8aab8762e499ec8da9d99b9afda7b4e693c
Ilham Mubarok <ilhmmbrok@gmail.com>
2026-04-21 09:53:43 +0700

    font family

--- src/pages/register.php
+++ src/pages/register.php
@@ -4,8 +4,15 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
+
+        body {
+            font-family: 'Inter', sans-serif;
+        }
+    </style>
     <link rel="stylesheet" href="../css/output.css">
+    <title>Login</title>
 </head>
 
 <body class="min-h-screen flex">
